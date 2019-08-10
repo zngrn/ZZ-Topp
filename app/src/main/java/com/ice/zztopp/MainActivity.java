4d56382693ca10ccd9b0678d6e0b5f962e72c2dd
vaibhav.jhingran <vaibhav.jhingran@go-jek.com>
2019-08-10 23:00:23 +0530

    WIP: added xml changes, a new song! and on click listeners

--- app/src/main/java/com/ice/zztopp/MainActivity.java
+++ app/src/main/java/com/ice/zztopp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ice.zztopp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.play_btn);
+        Button pauseButton = (Button) findViewById(R.id.pause_btn);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
